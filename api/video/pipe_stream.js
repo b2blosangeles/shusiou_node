b16a2af494bc1a7a46f65010c3ed6b3e23778c53
b2blosangeles <b2blosangeles@gmail.com>
2018-04-06 16:18:18 -0700

    Update pipe_stream.js

--- api/video/pipe_stream.js
+++ api/video/pipe_stream.js
@@ -39,7 +39,7 @@ let environment = (req.query['env']) ? ('_' + req.query['env']) : '',
     space_url = req.query['space'], 
     space_info = 'videos/' + source_file + '/_info.txt',
     space_dir = 'videos/' + source_file + '/_t/',
-    cache_folder = '/var/shusiou_cache/' + source_file + '/',
+    cache_folder = '/var/shusiou_cache/' + space_url.replace(/\//ig, '_') + '/' + source_file + '/',
     cache_info =  cache_folder + '_info.txt';
 
 var CP = new pkg.crowdProcess();
